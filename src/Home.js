063be3b763bd511753a7d12e741bca9ea76856da
Saurabh Singh <106748434+saurabhsingh720@users.noreply.github.com>
2024-12-24 11:23:16 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -23,7 +23,7 @@ export function Home() {
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
-      const res = axios.post("https://backend-hostel-complaint-website.vercel.app/api/v1/userIssue", data);
+      const res = axios.post("https://backend-hostel-complaint-website.onrender.com/api/v1/userIssue", data);
       toast.success("Issue created Successfully");
       console.log(res);
     } catch (err) {
